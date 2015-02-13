e3a26423cd4322f2963d6ebca7ec3b5d25ae702e
costd035 <daniel.x.costinett.-nd@disney.com>
2015-02-13 08:47:42 -0800

    fix tests -- userinfo,user -> userInfo

--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -17,7 +17,7 @@
 <%@ include file="header.jsp"%>
 
 <p>Welcome to <fmt:message key="title"/>, please enter your username and password</p>
-<form:form method="post" modelAttribute="userinfo">
+<form:form method="post" modelAttribute="userInfo">
 <div style="width:300px;text-align:right">
     <div class="formfield"><form:label path="name">Name:</form:label><form:input path="name"/><br></div>
     <div class="errors"><form:errors path="name" /></div>
--- src/test/java/com/springapp/mvc/AppTests.java
+++ src/test/java/com/springapp/mvc/AppTests.java
@@ -1,14 +1,20 @@
 package com.springapp.mvc;
 
+import net.cozz.danco.CartController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,7 +23,11 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml")
+@ContextConfiguration(locations = {
+        "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml",
+        "file:src/main/webapp/resources/data/books.xml",
+        "file:src/main/webapp/WEB-INF/spring/*.xml",
+        "file:src/main/webapp/WEB-INF/spring/appServlet/*.xml"})
 public class AppTests {
     private MockMvc mockMvc;
 
@@ -25,15 +35,29 @@ public class AppTests {
     @Autowired
     protected WebApplicationContext wac;
 
+    private MockHttpSession mockSession;
+
     @Before
     public void setup() {
-        this.mockMvc = webAppContextSetup(this.wac).build();
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        mockMvc = webAppContextSetup(wac).build();
+
+        mockSession = new MockHttpSession(null, UUID.randomUUID().toString());
     }
 
     @Test
     public void simple() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("hello"));
+                .andExpect(view().name("home"));
+    }
+
+    @Test
+    public void simpleCart() throws Exception {
+        mockMvc.perform(get("/cart"))
+                .andExpect(status().isOk());
     }
 }
